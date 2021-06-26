9f0c5f009ee5f63bb67ecdcb00886cdd896f8c14
WosaScripts <84085992+WosaScripts@users.noreply.github.com>
2021-06-26 20:09:33 +0200

    Update AnimeFightingSim.lua

--- AnimeFightingSim.lua
+++ AnimeFightingSim.lua
@@ -1,7 +1,7 @@
 
 game.StarterGui:SetCore("SendNotification", {Title = "System-Fludex", Text = "Open Te Loading String for te new script or join te discord server", Duration = 500})
-game.StarterGui:SetCore("SendNotification", {Title = "System-Fludex", Text = "Open Te Loading String for te new script or join te discord server", Duration = 500})
-game.StarterGui:SetCore("SendNotification", {Title = "System-Fludex", Text = "Open Te Loading String for te new script or join te discord server", Duration = 500})
+game.StarterGui:SetCore("SendNotification", {Title = "System-Fludex", Text = "join this \n https://discord.gg/vhKJnZkTTR", Duration = 500})
+game.StarterGui:SetCore("SendNotification", {Title = "System-Fludex", Text = "or dm me ,Woutt#4106", Duration = 500})
 
 --join or discord server : https://discord.gg/vhKJnZkTTR
 --new script : loadstring(game:HttpGet("https://raw.githubusercontent.com/Code-Fludex/Lua-Scripts/main/AnimeFightingSim.lua"))()
